7fbd220a6682814dfde8fa4c0e661a436ad4705f
Hannah Greene <hgreene2@wisc.edu>
2018-08-10 00:03:11 -0500

    Initial Commit

---
+++ Exercise 1-1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Exercise 1-1
+//
+//  Created by Hannah Greene on 8/10/18.
+//  Copyright © 2018 Hannah Greene. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
